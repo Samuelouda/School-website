eff9127bef3ab27167e789af98f74e541df93342
Anonymouse Baby <babyanonymouse2@gmail.com>
2023-08-11 10:15:23 +0300

    scss updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-let navb
-
-document.querySelector ('#menu-btn').onclick = () =>{
-
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-btn");
+    const navbar = document.querySelector(".navbar");
+  
+    menuButton.addEventListener("click", function () {
+      navbar.classList.toggle("show-menu");
+    });
+  });
+  
